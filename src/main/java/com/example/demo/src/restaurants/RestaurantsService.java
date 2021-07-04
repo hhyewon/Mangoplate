93e30f9d8d4ea3171363572da68da3bf623893e5
hhyewon <diddidgpdnjs@naver.com>
2021-07-05 01:44:24 +0900

    RESTFUL API 구현하기 (3)

--- src/main/java/com/example/demo/src/restaurants/RestaurantsService.java
+++ src/main/java/com/example/demo/src/restaurants/RestaurantsService.java
@@ -36,6 +36,7 @@ public class RestaurantsService {
     @Transactional
     public PostRestaurantRes createRestaurant(PostRestaurantReq postRestaurantReq) throws BaseException {
 
+
         try{
             System.out.println("2");
             int id = restaurantsDao.createRestaurant(postRestaurantReq);
